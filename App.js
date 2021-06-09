8430f904e08d502bcce007c2193f28bf69ed6336
shaksham08 <shakshamsinha@outlook.com>
2021-06-09 16:33:16 +0530

    Added custom Google fonts

--- App.js
+++ App.js
@@ -3,8 +3,24 @@ import React from "react";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  let [OswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  let [LatoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!OswaldLoaded || !LatoLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
